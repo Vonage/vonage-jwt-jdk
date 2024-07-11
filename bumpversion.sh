9487279f7ff7355a056fb78bb558384711950941
Sina Madani <sina.madani@vonage.com>
2024-07-11 11:35:37 +0100

    Tidy up POM

--- bumpversion.sh
+++ bumpversion.sh
@@ -6,6 +6,6 @@ fi
 
 mvn versions:set -DnewVersion=$1
 mvn validate
-#rm pom.xml.releaseBackup pom.xml.versionsBackup
+rm pom.xml.versionsBackup #pom.xml.releaseBackup
 #mvn versions:display-plugin-updates
 #mvn versions:display-dependency-updates
